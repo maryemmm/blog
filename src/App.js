917e77b613d6ce75ea2257b4d0dac777b0d74b84
maryemmm <maryemsakhry@gmail.com>
2024-02-22 22:11:45 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="center">
+        <img src="https://res.cloudinary.com/dgci0ivas/image/upload/v1684534060/Image_1_g5mxyi.png" alt=""/>
+        <h3>Blog post published</h3>
+        <p>This post has been pablished.Team members will be able to edit this post and republish changes.</p>
+        <button type="submit" className="cancel">Cancel</button>
+        <button type="submit" className="confirm">Confirm</button>
     </div>
   );
 }
 
 export default App;
+
